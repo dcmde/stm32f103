f8b49fbc63bb55dd0f02bd45bd1a496f80ee6acf
dcmde <43622430+dcmde@users.noreply.github.com>
2021-05-17 22:23:23 +0200

    Working example to write data on the SD card.

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ In my code I use the filesystem module available [here](http://elm-chan.org/fsw/
 One of the most important thing to do when using this code is to take care of calling the `disk_timerproc()` every 10ms.
 In my example I use a timer to do so.
 
+WARNING : The microSd card adapter should have a 5V tension supply.
--- src/microSdTask.cpp
+++ src/microSdTask.cpp
@@ -1,29 +1,51 @@
 #include <misc.h>
+#include <spi.h>
+#include <cstdio>
+#include <gpio.hpp>
 #include "microSdTask.hpp"
 #include "timer.hpp"
-#include "gpio.hpp"
+#include "ff.h"
+#include "diskio.h"
 
 extern "C" {
 void TIM2_IRQHandler(void) {
     if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
         TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-        if (Gpio::readPin(A6)) {
-            Gpio::pinLow(A6);
-        } else {
-            Gpio::pinHigh(A6);
-        }
+        disk_timerproc();
     }
 }
 }
 
+FATFS FatFs;
+FIL fil;
+
 [[noreturn]] void microSdTask(void *p) {
 
-    Gpio::setPin(A6, GPIO_Mode_Out_PP);
+    // Configure 10ms interrupt.
     Timer::set(TIM2, Div_1, 7200, 100, Up);
     Timer::setTimerInterrupt(TIM2, TIM2_IRQn, Update);
+    Gpio::setPin(C13, GPIO_Mode_Out_PP);
 
+    init_spi(SPI1, 0);
+    vTaskDelay(1000);
+
+    if (f_mount(&FatFs, "0", 1) == FR_OK) {
+        if (f_open(&fil, "1stfile.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK) {
+            if (f_puts("First string in my file\n", &fil) > 0) {
+                printf("write\n");
+            }
+            f_close(&fil);
+        }
+        f_mount(0, "", 1);
+    }
     // main loop
     while (1) {
         vTaskDelay(1000);
+        Gpio::pinHigh(C13);
+        spi_slave_enable();
+        vTaskDelay(1000);
+        Gpio::pinLow(C13);
+        spi_slave_disable();
+
     }
 }
