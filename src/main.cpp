af771f3a52552b8cbf8d76afbc64684543801cfb
dcmde <43622430+dcmde@users.noreply.github.com>
2021-06-05 16:02:57 +0200

    Working version with printf.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
-#include <FreeRTOS.h>
-#include <task.h>
-#include <task/microSdTask.hpp>
-#include <task/timerTask.hpp>
+#include <cstdio>
 
 int main() {
-    xTaskCreate(microSdTask, "microSd", 128, nullptr, 1, nullptr);
-    xTaskCreate(timerTask, "timer", 128, nullptr, 1, nullptr);
-    vTaskStartScheduler();
+    printf("hello\n");
     return 0;
 }
